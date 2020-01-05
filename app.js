ddb10c4532b8126c2b2bfa4ef3923561a1179ed1
jwkim <wlsdhks0423@naver.com>
2020-01-05 19:16:50 +0900

    CORS설정 추가

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+//CORS 허용 설정
+app.use(cors());
+
+//서버 구축 테스트용
 app.get('/', function(req, res) {
     res.send({hello:'world'});
 });
 
-app.listen(3000, function() {
-    console.log('Server listening on Port 3000 . . .');
+//서버 구축 테스트용
+app.listen(80, function() {
+    console.log('Server listening on Port 80 . . .');
 });
