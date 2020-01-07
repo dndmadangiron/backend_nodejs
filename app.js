40945596d1b98f79e7e76487b9a71c12d26f7ec5
jwkim <wlsdhks0423@naver.com>
2020-01-07 23:16:19 +0900

    아이디 중복체크 구현

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
+const PORT = 3030;
 
-//CORS 허용 설정
-app.use(cors());
+/**[START] 모듈 선언*/
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const logger = require('./config/winston');
+/**[END] 모듈 선언*/
 
-//서버 구축 테스트용
-app.get('/', function(req, res) {
-    res.send({hello:'world'});
-});
+/**[START] 모듈 적용*/
+app.use(cors());            //CORS 허용 설정
+app.use(bodyParser.json()); //request body로 넘어오는 데이터를 json으로 변환
+app.use(bodyParser.urlencoded({extended:true}));
+/**[END] 모듈 적용*/
 
-//서버 구축 테스트용
-app.listen(3030, function() {
-    console.log('Server listening on Port 3000 . . .');
+/**[START] 기타 설정*/
+//app.use(express.static('public/js'));//static한 요소들 저장
+/**[END] 기타 설정*/
+
+
+/**[START] router 선언영역*/
+let users = require('./routes/user');
+/**[END] router 파일 선언영역*/
+
+/**[START] router 적용영역*/
+app.use('/users', users);
+/**[END] router 파일 적용영역*/
+
+
+//서버 start
+app.listen(PORT, function() {
+    logger.info('Server listening on '+PORT+' . . .');
 });
---
+++ config/winston.js
@@ -0,0 +1,59 @@
+const rootPath = require('app-root-path');
+const winston = require('winston');
+const process = require('process');
+
+const { combine, timestamp, label, printf } = winston.format;
+
+const customFormat = printf(({level, message, timestamp}) => {
+    return `${timestamp} ${level}: ${message}`;
+});
+
+let date = new Date();
+let today = '';
+today += date.getFullYear(); 
+today += (date.getMonth().length == 1) ? '0'+date.getMonth() : date.getMonth();
+today += (date.getDate().length == 1) ? '0'+date.getDate() : date.getDate();
+
+
+const options = {
+    file: {
+        level: 'info',
+        filename: `${rootPath}/logs/${today}.log`,
+        handleExceptions: true,
+        json: false,
+        maxSize: null,
+        maxFiles: 60,
+        colorize: false,
+        format: combine(
+            timestamp({
+                format: 'YYYY-MM-DD HH:mm:ss',
+            }),
+            customFormat
+        )
+    },
+    console: {
+        level: 'debug',
+        handleExceptions: true,
+        json: false,
+        colorize: true,
+        format: combine(
+            timestamp({
+                format: 'YYYY-MM-DD HH:mm:ss',
+            }),
+            customFormat
+        )
+    }
+}
+
+let logger = new winston.createLogger({
+    transports: [
+        new winston.transports.File(options.file),
+    ],
+    exitOnError: false,
+});
+
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console(options.console));
+}
+
+module.exports = logger;
