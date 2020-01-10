c2223f1fd8a565df75e326cc7e7934161bcb5c11
JinwooKim <39347051+uukk960@users.noreply.github.com>
2020-01-10 15:49:20 +0900

    login 구현 - JINU

--- app.js
+++ app.js
@@ -34,6 +34,15 @@ let users = require('./routes/user');
 app.use('/users', users);
 /**[END] router 파일 적용영역*/
 
+/**[START] login 파트*/
+/** Develop comment
+ * 나중에 완전히 정리되면, router선언 및 적용 영역으로 이동할 예정
+ * (login.js)와 (routes/user.js)의 환경 설정 부분 겹치는게 많으므로 합치는 것도 고려해볼것
+ */
+let login = require('./login.js');
+app.use('/auth/login', login);
+
+/**[END] login 파트*/
 
 //서버 start
 app.listen(PORT, function() {
