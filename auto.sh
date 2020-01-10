61a350f56a78d9465293f2c1a85da1c743e3dbb4
jwkim <wlsdhks0423@naver.com>
2020-01-11 03:26:02 +0900

    edit .gitignore

--- auto.sh
+++ auto.sh
@@ -3,14 +3,15 @@
 # 0. timezone setting(Asia/Seoul)
 # sudo ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
 # 1. git pull
+echo "cd /home/ssh_user/server"
+OUTPUT0="$(cd /home/ssh_user/server)"
+echo "${OUTPUT0}"
+
+
 echo "> git pull"
-OUTPUT="$(git pull)"
-#if [$? -eq 0]; then
-#	echo "git pull SUCCESS"
-#else 
-#	echo "git pull FAIL"
-#fi
-echo "${OUTPUT}"
+OUTPUT1="$(git pull)"
+
+echo "${OUTPUT1}"
 
 
 
@@ -18,11 +19,7 @@ echo "${OUTPUT}"
 # 2. npm install (install project dependency)i
 echo "> npm install"
 OUTPUT2="$(npm install)"
-#if [$? -eq 0]; then
-#	echo "npm install SUCCESS"
-#else 
-#	echo "npm install FAIL"
-#fi
+
 echo "${OUTPUT2}"
 
 
@@ -33,12 +30,6 @@ echo $(pm2 kill)
 APP_START="pm2 start --name=\"app\" app.js --watch --ignore-watch=\"logs/*\""
 echo "> ${APP_START}"
 OUTPUT3="$(${APP_START})"
-#OUTPUT3="$(pm2 restart app --watch --ignore-watch=\"logs/*\")"
-#if [$? -eq 0]; then
-#	echo "app restart SUCCESS"
-#else
-#	echo "app restart FAIL"
-#if
 
 echo "${OUTPUT3}"
 
