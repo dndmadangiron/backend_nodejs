dccad706ee0ff26a80373b400ed36b83bba14a71
tkakcy159 <tkakcy159@gmail.com>
2020-05-17 14:26:40 +0900

    fix parameters

--- auto.sh
+++ auto.sh
@@ -10,20 +10,20 @@ sudo ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
 
 # 1. move to project directory
 move_to_prj="cd /node-app/server"
-run_cmd move_to_prj
+run_cmd ${move_to_prj}
 
 # 2. git pull
 git_pull="git pull"
-run_cmd git_pull
+run_cmd ${git_pull}
 
 # 3. npm install (install project dependency)i
 npm_install="npm install"
-run_cmd npm_install
+run_cmd ${npm_install}
 
 # 4. restart pm2 (app)
 pm2_kill="pm2 kill"
-run_cmd pm2_kill
+run_cmd ${pm2_kill}
 
 APP_START="pm2 start --name=\"app\" app.js --watch --ignore-watch=\"logs/*\""
-run_cmd APP_START
+run_cmd ${APP_START}
 
